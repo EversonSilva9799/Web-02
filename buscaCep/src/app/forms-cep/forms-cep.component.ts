284b2b08146a0559dc1501ab27e62f2eb62537e6
Everson Silva <eversonsilva9799@gmail.com>
2019-10-16 16:05:25 -0300

    Criado model para representar um Endereco. Criado tela de exibir históricos já pesquisados e pesquisa instantânea

--- buscaCep/src/app/forms-cep/forms-cep.component.ts
+++ buscaCep/src/app/forms-cep/forms-cep.component.ts
@@ -1,6 +1,7 @@
-import { ServicoCepService } from './../servico-cep.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { CepService } from '../services/cep.service';
+import { Endereco } from '../models/Endereco';
 
 @Component({
 	selector: 'app-forms-cep',
@@ -8,27 +9,45 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 	styleUrls: [ './forms-cep.component.css' ]
 })
 export class FormsCepComponent implements OnInit {
-	formulario: FormGroup;
+	public cep: string;
+	public endereco: Endereco;
+	public formulario: FormGroup;
 
-	constructor(private fb: FormBuilder, private servico: ServicoCepService) {}
+	constructor(private fb: FormBuilder, private cepService: CepService) {
+		this.endereco = new Endereco();
+	}
 
 	ngOnInit() {
 		this.formulario = this.fb.group({
 			cep: [ null, [ Validators.required, Validators.minLength(8), Validators.maxLength(8) ] ],
-			logradouro: [ null ],
-			bairro: [ null ],
-			cidade: [ null ],
-			uf: [ null ]
+			logradouro: [ { value: null, disabled: true } ],
+			bairro: [ { value: null, disabled: true } ],
+			cidade: [ { value: null, disabled: true } ],
+			uf: [ { value: null, disabled: true } ]
+		});
+
+		this.formulario.controls.cep.valueChanges.subscribe((cep) => {
+			if (cep.length === 8) {
+				this.buscar();
+			}
 		});
 	}
 
-	buscar() {
+	public buscar() {
 		const cep = this.formulario.controls.cep.value;
-		this.servico.consultaCep(cep).subscribe((dados) => {
+		this.cepService.consultaCep(cep).subscribe((dados) => {
 			this.formulario.controls.cidade.setValue(dados['localidade'] || 'Não disponível');
 			this.formulario.controls.bairro.setValue(dados['bairro'] || 'Não disponível');
 			this.formulario.controls.logradouro.setValue(dados['logradouro'] || 'Não disponível');
 			this.formulario.controls.uf.setValue(dados['uf'] || 'Não disponível');
+
+			this.endereco.cep = cep;
+			this.endereco.cidade = dados['localidade'];
+			this.endereco.bairro = dados['bairro'];
+			this.endereco.logradouro = dados['logradouro'];
+			this.endereco.uf = dados['uf'];
+
+			this.cepService.armazenarPesquisa(this.endereco);
 		});
 	}
 }
--- buscaCep/src/app/historico-pesquisa/historico-pesquisa.component.ts
+++ buscaCep/src/app/historico-pesquisa/historico-pesquisa.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { CepService } from '../services/cep.service';
+import { Endereco } from '../models/Endereco';
 
 @Component({
-  selector: 'app-historico-pesquisa',
-  templateUrl: './historico-pesquisa.component.html',
-  styleUrls: ['./historico-pesquisa.component.css']
+	selector: 'app-historico-pesquisa',
+	templateUrl: './historico-pesquisa.component.html',
+	styleUrls: [ './historico-pesquisa.component.css' ]
 })
 export class HistoricoPesquisaComponent implements OnInit {
+	public enderecos: Endereco[] = [];
 
-  constructor() { }
+	constructor(private cepService: CepService) {
+		this.getHistorico();
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	public getHistorico() {
+		this.enderecos = this.cepService.getHistorico();
+	}
 }
